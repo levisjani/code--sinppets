99968efe831b5c2f92ebc69c4cf925f9a13d1242
Levis Jani <levisjani@rocketmail.com>
2014-08-24 19:45:25 +0200

    Create string.cpp

---
+++ cpp/string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+
+    string inputString;
+    cout << "Input a string: \n";
+    getline(cin,inputString);
+    cout << "The string is: " << inputString << endl;
+    return 0;
+}
