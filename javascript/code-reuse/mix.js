b4eeacd8c325b540b7a7e8d3c1048395853431a2
Levis Jani <levisjani@rocketmail.com>
2014-08-27 22:53:14 +0200

    Mix

---
+++ javascript/code-reuse/mix.js
@@ -0,0 +1,27 @@
+<script>
+			/* Title: Mix-ins
+			 Description: copy from any number of objects and mix them all into a new object
+			 */
+
+			function mix() {
+				var arg, prop, child = {};
+				for (arg = 0; arg < arguments.length; arg += 1) {
+					for (prop in arguments[arg]) {
+						if (arguments[arg].hasOwnProperty(prop)) {
+							child[prop] = arguments[arg][prop];
+						}
+					}
+				}
+				return child;
+			}
+
+			var cake = mix(
+					{eggs:2, large:true},
+					{butter:1, salted:true},
+					{flour:'3 cups'},
+					{sugar:'sure!'}
+			);
+
+			console.dir(cake);
+			
+</script>			
