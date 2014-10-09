f18599ad6ce6b559d31bfb56e627f40f044f94d5
Levis Jani <levisjani@rocketmail.com>
2014-10-09 22:33:55 +0200

    Create start.c

---
+++ linux/kernel-modules/start.c
@@ -0,0 +1,14 @@
+/*
+ *  start.c - Illustration of multi filled modules
+ */
+ 
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+include init_module(void)
+{
+  
+  printk(KERN_INFO "Kernel reply - kernel loopback test\n);
+  return 0;
+  
+}
