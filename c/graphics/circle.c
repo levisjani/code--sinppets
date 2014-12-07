7007cd71dbb20f5951ac2702e12ec8fedf65b2c4
Levis Jani <levisjani@rocketmail.com>
2014-12-07 21:01:31 +0100

    Create circle.c

---
+++ c/graphics/circle.c
@@ -0,0 +1,15 @@
+#include<graphics.h>
+#include<conio.h>
+ 
+main()
+{
+   int gd = DETECT, gm;
+ 
+   initgraph(&gd, &gm, "C:\\TC\\BGI");
+ 
+   circle(100, 100, 50);
+ 
+   getch();
+   closegraph();
+   return 0;
+}
