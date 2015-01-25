1b025b582347fedc8ba205a42719955e2f98403a
Levis Jani <levisjani@rocketmail.com>
2015-01-25 21:09:35 +0100

    Update Composer.java

--- java/j2ee/ajax/Composer.java
+++ java/j2ee/ajax/Composer.java
@@ -1,10 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package com.ajax;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
