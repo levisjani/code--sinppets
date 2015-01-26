a39f32edb83873364ed0e66c1bbe0f9fc0cf2e5c
Levis Jani <levisjani@rocketmail.com>
2015-01-27 00:40:47 +0100

    Update Film.java

--- java/j2ee/jpa/Film.java
+++ java/j2ee/jpa/Film.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package testjpa;
-
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Collection;
@@ -30,7 +23,7 @@ import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author elton.ballhysa
+ * @author levis.jani
  */
 @Entity
 @Table(name = "film")
