0b22eb5bbb470eb9d8e99ad188c677eaae56f787
Levis Jani <levisjani@rocketmail.com>
2015-01-27 00:40:19 +0100

    Update Actor.java

--- java/j2ee/jpa/Actor.java
+++ java/j2ee/jpa/Actor.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package testjpa;
-
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
@@ -27,7 +20,7 @@ import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author elton.ballhysa
+ * @author levis.jani
  */
 @Entity
 @Table(name = "actor")
