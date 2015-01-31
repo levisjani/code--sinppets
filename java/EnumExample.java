753803e1b0d2d1985a5181037103185511e1bebd
Levis Jani <levisjani@rocketmail.com>
2015-01-31 20:59:20 +0100

    Create EnumExample.java

---
+++ java/EnumExample.java
@@ -0,0 +1,17 @@
+public class EnumExample {
+ 
+	public enum Company {
+		EBAY(30), PAYPAL(10), GOOGLE(15), YAHOO(20), ATT(25);
+		private int value;
+ 
+		private Company(int value) {
+			this.value = value;
+		}
+	}
+ 
+	public static void main(String[] args) {
+		for (Company cName : Company.values()) {
+			System.out.println("Company Value: " + cName.value + " - Comapny Name: " + cName);
+		}
+	}
+}
