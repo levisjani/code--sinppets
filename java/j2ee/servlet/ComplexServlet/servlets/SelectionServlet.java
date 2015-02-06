68ecccded84ae7ef431785d25ad3cc1cfafbae9b
Levis Jani <levisjani@rocketmail.com>
2015-02-06 22:43:18 +0100

    Update SelectionServlet.java

--- java/j2ee/servlet/ComplexServlet/servlets/SelectionServlet.java
+++ java/j2ee/servlet/ComplexServlet/servlets/SelectionServlet.java
@@ -1,10 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package unyt.servlets;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -17,10 +10,7 @@ import javax.servlet.http.HttpSession;
 import unyt.utils.General;
 import unyt.utils.Item;
 
-/**
- *
- * @author elton
- */
+
 public class SelectionServlet extends HttpServlet {
    
     /** 
