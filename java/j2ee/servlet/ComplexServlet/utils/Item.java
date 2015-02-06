a67ca3c77e097003ec122e068dfd79c832179f2f
Levis Jani <levisjani@rocketmail.com>
2015-02-06 22:45:46 +0100

    Create Item.java

---
+++ java/j2ee/servlet/ComplexServlet/utils/Item.java
@@ -0,0 +1,65 @@
+public class Item {
+    private String id;
+    private String name;
+    private double unitPrice;
+
+    public Item(String id, String name, double unitPrice) {
+        setId(id);
+        setName(name);
+        setUnitPrice(unitPrice);
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the unitPrice
+     */
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    /**
+     * @param unitPrice the unitPrice to set
+     */
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+
+        if (! (obj instanceof Item))
+            return false;
+
+        Item itObj = (Item) obj;
+        return this.getId().equals(itObj.getId());
+    }
+}
